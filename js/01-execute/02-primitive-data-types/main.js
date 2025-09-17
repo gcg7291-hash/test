627ae3be66e9547a3c5dec32e6f3b01f9a4d07ec
gcg7291-hash <gcg7291@gmail.com>
2025-09-17 09:58:22 +0900

    js

---
+++ js/01-execute/02-primitive-data-types/main.js
@@ -0,0 +1,18 @@
+console.log("Hello World dd");
+
+// 주석 생성//
+// ctrl + / : 주석생성
+
+// 자료형 확인 키워드 (예약어)
+// typeof 확인하는 데이터
+console.log(typeof "Hello World"); //string
+
+// 개발 입문자라면 데이터의 자료형을 자주 확인하는 습관이 필요 typeof <
+
+console.log(typeof 42);
+
+// 원시 자료형 5개 (총 7개)
+// 문자열 (string)
+// 문자들의 나열 : 0개 이상의 문자를 나타내는 자료형
+// 빈문자열("")도 문자열
+console.log(typeof "");
---
+++ js/01-execute/main.js
@@ -0,0 +1,4 @@
+console.log("Hello world")
+console.log('반갑습니다')
+console.log(`좋은 하루 되세요`)
+console.log(42)
