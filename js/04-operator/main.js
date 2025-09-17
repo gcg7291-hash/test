e0ae46a7fe1541c386bb75b7d856d1b1d5033a5a
gcg7291-hash <gcg7291@gmail.com>
2025-09-17 11:48:46 +0900

    js

---
+++ js/04-operator/main.js
@@ -0,0 +1,25 @@
+// 논리연산자
+// 논리형(불리언형, true/false)을 기반으로한 연산자
+const trueData = true;
+const falseData = false;
+// && (AND)
+// 2개의 논리형 데이터가 참이면 참 (true) 생성
+// 1개라도 논리형 데이터가 거짓이면 거짓 (false) 생성
+
+// 템플릿 리터럴에는 변수뿐만 아니라 표현식을 삽입 가능
+// 표현식 : 데이터를 생성하는 코드
+console.log(`ture && true -> ${trueData && trueData}`);
+console.log(`false && true -> ${falseData && tureData}`);
+
+// || (OR)
+// 1개의 논리형 데이터가 참이면 참 (true) 생성
+// 2개의 논리형 데이터가 거짓이면 거짓 (false) 생성
+console.log(`true || false -> ${true || false}`);
+console.log(`false || false -> ${false || false}`);
+
+// ! (NOT)
+// 1개의 논리형 데이터만 필요
+// 논리형 데이터가 거짓이면 참 (true) 생성
+// 논리형 데이터가 참이면 거짓 (false) 생성
+console.log(`!ture -> ${!true}`);
+console.log(`!false -> ${!false}`);
