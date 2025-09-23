54fc38c3c4f42c85c3430f55fb9a802479f0c16a
gcg7291-hash <gcg7291@gmail.com>
2025-09-23 14:40:50 +0900

    promise

--- js/15-promise/main.js
+++ js/15-promise/main.js
@@ -2,18 +2,18 @@
 // new Promise() : 생성자 함수
 // (resolve, reject) => {} : 비동기 작업을 수행하고, 결과를 반환하는 함수
 
-// const randomPromise = new Promise((resolve, reject) => {
-//   // 비동기 작업 흉내
-//   setTimeout(() => {
-//     const random = Math.random(); // 랜덤 숫자 생성
+const randomPromise = new Promise((resolve, reject) => {
+  // 비동기 작업 흉내
+  setTimeout(() => {
+    const random = Math.random(); // 랜덤 숫자 생성
 
-//     if (random > 0.5) {
-//       resolve("숫자가 0.5 이상! 성공"); // 성공 시  반환
-//     } else {
-//       reject("숫자가 0.5 이하! 실패"); // 실패 시  반환
-//     }
-//   }, 1000);
-// });
+    if (random > 0.5) {
+      resolve("숫자가 0.5 이상! 성공"); // 성공 시  반환
+    } else {
+      reject("숫자가 0.5 이하! 실패"); // 실패 시  반환
+    }
+  }, 1000);
+});
 
 // promise 두 가지의 상태
 // 비동기 처리의 성공과 실패
@@ -37,11 +37,41 @@
 
 // fetch : 네트워크 통신 함수
 // 필수품 : 웹 서비스의 주소
-fetch("https://example.com")
-  .then((response) => {
-    console.log(response);
-    return response.text();
-  })
-  .then((data) => {
-    console.log(data);
-  });
+// fetch("https://example.com")
+//   .then((response) => {
+//     console.log(response);
+//     return response.text();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   });
+
+// async / await
+
+// Promise 자료형을 쉽게 사용하게 도와주는 키워드
+
+// async 키워드
+// 함수앞에 붙는 키워드.
+// 해당 함수가 비동기를 처리하는 함수란것을 표시
+
+async function func() {
+  // await 키웓
+  // Promise의 완료(성공 or 실패) 를 대기하는 키워드
+  try {
+    // 비동기 처리가 수행하는 코드 블록
+
+    // await 키워드
+    // Promise 기반 비동기처리 수행
+    // 수행 결과 resolve() 함수 인자를 반환
+    const result = await randomPromise;
+    console.log(result);
+    await randomPromise;
+  } catch (error) {
+    // 비동기처리가 실패했을때
+    console.log(error);
+  }
+}
+func();
+
+// 비동기 처리 연속 async await fetch / const data = async ~~
+
