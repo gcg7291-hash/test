7b880f0a9973b0b54532e23c7fa47df356730fee
gcg7291-hash <gcg7291@gmail.com>
2025-09-23 15:22:33 +0900

    web-api

---
+++ js/16-web-api/main.js
@@ -0,0 +1,13 @@
+// async / await
+
+// axios 불러오기
+import axios from "axios";
+async function connectTest() {
+  // axios.get(url) : 데이터 요청에 대한 응답 데이터 을 반환
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  console.log(response["data"]);
+  // console.table(Object.keys(response));
+}
+connectTest();
