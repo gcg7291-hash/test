da7b6d9a088cbe158fd75d7f5c0ff1e9191e3718
gcg7291-hash <gcg7291@gmail.com>
2025-10-15 15:17:31 +0900

    리덕스 기초

---
+++ react/redux-app/src/components/CounterIncrementByAmount.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+// 사용자에게 값을 입력받고, 입력 값 만큼 전역상태 count 에 더하기
+import { useState } from "react";
+// 전역 상태를 변경하기 위해 필요한 두 가지 모듈
+// useDispatch 훅
+import { useDispatch } from "react-redux";
+// 액션 생성자 함수
+import { incrementByAmount } from "../store/counterSlice";
+
+export default function CounterIncrementByAmount() {
+  const dispatch = useDispatch();
+
+  // 지역상태 payload 목적: 사용자 입력 값 관리
+  const [a, setPayLoad] = useState(0);
+
+  return (
+    <div>
+      <input
+        type="number"
+        value={a}
+        onChange={(e) => {
+          setPayLoad(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          // 액션 생성자 함수의 인자 === payload(외부 전달 값, 상태를 얼마나 변경할지 결정하는 값)
+          
+          dispatch(incrementByAmount(a));
+        }}
+      >
+        증가
+      </button>
+    </div>
+  );
+}
